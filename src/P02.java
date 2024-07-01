1082034ad4337a485d271e4f0707978c4edf7e04
gulnarjbl <gulnar-jabrayilova@hotmail.com>
2024-07-01 10:41:46 +0300

    v2 yeni1

--- src/P02.java
+++ src/P02.java
@@ -1,5 +1,9 @@
 public class P02 {
     public static void main(String[] args) {
         System.out.println("ikinci dosya ekle");
+        System.out.println("deneme versiyonu");
+        int s=25;
+        int b=52;
+        System.out.println("toplam"+(s+b));
     }
 }
